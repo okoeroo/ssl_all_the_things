345d87761a562ce17f0733e9447e703c48ec2b3c
Oscar Koeroo <okoeroo@gmail.com>
2012-04-27 17:26:13 +0200

    Initial downloader is working

---
+++ download_ssl.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+
+#echo -e "GET / \n\n" | openssl s_client -cipher `openssl ciphers` -tlsextdebug -connect localhost:8443 -state
+BOGUS_HTTP_REQ="echo -e \"GET / \n\n\""
+
+
+if [ -z "$1" ]; then
+    echo "No connection string passed. Use: dns.name.tls:443" 1>&2
+    exit 1
+else
+    CONNECT_STRING="$1"
+fi
+
+
+echo "Connecting to: ${CONNECT_STRING}"
+
+$BOGUS_HTTP_REQ | \
+    openssl s_client \
+    -cipher `openssl ciphers` \
+    -tlsextdebug \
+    -state \
+    -connect $CONNECT_STRING
+
+
+RC=$?
+
+
+exit ${RC}
+
